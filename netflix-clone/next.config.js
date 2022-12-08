9a79c813debc830000f76105609691e6f17d4cc5
unknown <bousilmustapha@gmail.com>
2022-12-08 20:31:27 +0100

    add design login

--- netflix-clone/next.config.js
+++ netflix-clone/next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images:{
-    domains:['image.tmdb.org'],/*mli dir hade reload server */
+    domains:['image.tmdb.org','rb.gy'],/*mli dir hade reload server */
   },
 }
