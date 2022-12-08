20ffb712ab03530726e2a2f8219a115ba0ec6e3b
unknown <bousilmustapha@gmail.com>
2022-12-08 23:30:55 +0100

    add auth provider fon register and login and logout

---
+++ netflix-clone/hooks/useAuth.tsx
@@ -0,0 +1,84 @@
+import React, { useState,useEffect,useMemo,useContext,createContext, Children } from 'react'
+import {
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+    User 
+} from 'firebase/auth'
+
+import { useRouter } from 'next/router'
+import {auth} from '../firebase'
+
+interface IAuth{
+    user : User | null 
+    signUp:(email:string,password:string) => Promise<void>//return promise
+    signIn:(email:string,password:string) => Promise<void>//return promise 
+    logout:()=> Promise<void>
+    error:string | null 
+    loading:boolean 
+}
+const AuthContext=createContext<IAuth>({
+    user:null ,
+    signUp:async ()=>{},
+    signIn : async ()=>{},
+    logout:async ()=>{},
+    error:null ,
+    loading :false 
+})
+
+
+
+interface AuthProviderProps{
+    children:React.ReactNode 
+}
+export const AuthProvider = ({children} : AuthProviderProps) => {
+    const [loading,setLoading]=useState(false)
+    const [user,setUser]=useState<User | null>(null /*par difault*/)
+    const [error,setError]=useState(null)
+    const [initalLoading,setInitialLoading]=useState(true)
+    const router=useRouter()
+  const signUp=async (email:string,password:string)=>{
+    setLoading(true)
+    await createUserWithEmailAndPassword(auth,email,password)
+    .then((userCredential)=>{
+        setUser(userCredential.user)
+        router.push('/')
+        setLoading(false)
+    }).catch((error)=>alert(error.message)).finally(()=> setLoading(false))
+  }
+
+  const signIn=async (email:string,password:string)=>{
+    setLoading(true)
+    await signInWithEmailAndPassword(auth,email,password)
+    .then((userCredential)=>{
+        setUser(userCredential.user)
+        router.push('/')
+        setLoading(false)
+    }).catch((error)=>alert(error.message)).finally(()=> setLoading(false))
+  }
+
+  const logout=async ()=>{
+    setLoading(true)
+    signOut(auth).then(()=>{
+        setUser(null)
+    })
+    .catch((error)=>alert(error.message))
+    .finally(()=>setLoading(false))
+  }
+
+
+  const memoedValue=useMemo(()=>({
+    user,signUp ,signIn,loading,logout,error
+  }),[user,loading])
+  return (
+    <AuthContext.Provider value={memoedValue}>
+    {children}
+   </AuthContext.Provider>
+  )
+
+}
+
+export default function useAuth(){
+    return useContext(AuthContext)
+}
